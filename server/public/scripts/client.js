bc001c7004f07dd1b84d0b7aaa6cb14c3aca9374
bryant.nupdal <nupdalbryant@icloud.com>
2022-01-20 16:32:36 -0600

    completed base

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,6 +8,8 @@ function addClickHandlers() {
   $('#submitBtn').on('click', handleSubmit);
 
   // TODO - Add code for edit & delete buttons
+  $('#bookShelf').on('click', '.delete-button', deleteBooks);
+  $('#bookShelf').on('click', '.isRead-button', isRead);
 }
 
 function handleSubmit() {
@@ -31,6 +33,42 @@ function addBook(bookToAdd) {
       console.log('Error in POST', error)
       alert('Unable to add book at this time. Please try again later.');
     });
+}
+
+function deleteBooks(event){
+  console.log('made it to the delete function');
+  const button = $(event.target);
+  const bookid = button.data('bookid');
+  console.log(bookid);
+  $.ajax({
+    method: 'DELETE',
+    url: `/books/${bookid}`
+  }).then((response) => {
+    refreshBooks();
+  }).catch((error) => {
+    console.log('Error deleting book: ',error);
+  })
+}
+
+function isRead (event){
+  console.log('made it to is read function');
+  const button = $(event.target);
+  bookid = button.data('readid');
+  let read = button.data('isread');
+  console.log(`${read}`);
+
+
+  $.ajax({
+    method: 'PUT', 
+    url: `/books/read/${bookid}`,
+    data: {
+      isRead: `${read}`
+    }
+  }).then (() => {
+    refreshBooks();
+  }).catch(error => {
+    console.log('error updating', error);
+  })
 }
 
 // refreshBooks will get all books from the server and render to page
@@ -58,6 +96,9 @@ function renderBooks(books) {
       <tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
+        <td>${book.isRead}</td>
+        <td><button class="isRead-button" data-readid='${book.id}' data-isRead='${book.isRead}'>Read</button></td>
+        <td><button class="delete-button" data-bookid='${book.id}'>Delete</button></td>
       </tr>
     `);
   }
--- server/routes/book.router.js
+++ server/routes/book.router.js
@@ -38,11 +38,38 @@ router.post('/',  (req, res) => {
 // Updates a book to show that it has been read
 // Request must include a parameter indicating what book to update - the id
 // Request body must include the content to update - the status
-
+router.put('/read/:id', (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  let queryText ='';
+  console.log(req.body.isRead);
+  
+  if (req.body.isRead === 'true' ){
+    queryText = `UPDATE "books" SET "isRead"='false' WHERE id=$1`;
+  }else{
+    queryText = `UPDATE "books" SET "isRead"='true' WHERE id=$1`;
+  }
+  pool.query(queryText, [req.params.id]).then(() => {
+     res.sendStatus(200);
+  }).catch((error) => {
+     console.log(error);
+     res.sendStatus(500);
+  })
+})
 
 // TODO - DELETE 
 // Removes a book to show that it has been read
 // Request must include a parameter indicating what book to update - the id
-
+router.delete('/:id',(req,res) => {
+  console.log(req.params);
+  const bookid = req.params.id;
+  const queryText = 'DELETE FROM "books" WHERE id=$1;';
+  pool.query(queryText, [bookid]).then((result) => {
+    res.sendStatus(204);
+  }).catch((error) => {
+    console.log(error);
+    res.sendStatus(500);
+  })
+});
 
 module.exports = router;
